c429387871061a08c325ddbb9368aac230218d02
Iliya333 <69747853+Iliya333@users.noreply.github.com>
2020-12-28 21:51:59 +0500

    перенес сессию из методов в поле

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -11,31 +11,30 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
-import java.util.Queue;
+
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
     }
 
+    Session session = Util.getSessionFactory().openSession();
 
     @Override
     public void createUsersTable() {
+        Transaction transaction = session.beginTransaction();
+        String sql = "CREATE TABLE IF NOT EXISTS USERS\n" +
+                "(\n" +
+                "\tID int NOT NULL  AUTO_INCREMENT,\n" +
+                "\tNAME varchar(255) not null,\n" +
+                "\tLASTNAME varchar(255) not null,\n" +
+                "\tAGE TINYINT not null,\n" +
+                "\tconstraint USERS_pk\n" +
+                "\t\tprimary key (ID)\n" +
+                ");";
         try {
-            Session session = Util.getSessionFactory().openSession();
-            Transaction transaction = session.beginTransaction();
-            String sql = "CREATE TABLE IF NOT EXISTS USERS\n" +
-                    "(\n" +
-                    "\tID int NOT NULL  AUTO_INCREMENT,\n" +
-                    "\tNAME varchar(255) not null,\n" +
-                    "\tLASTNAME varchar(255) not null,\n" +
-                    "\tAGE TINYINT not null,\n" +
-                    "\tconstraint USERS_pk\n" +
-                    "\t\tprimary key (ID)\n" +
-                    ");";
             session.createSQLQuery(sql).executeUpdate();
             transaction.commit();
-            session.close();
             System.out.println("Таблица создана.");
         } catch (HibernateException e) {
             e.printStackTrace();
@@ -47,11 +46,9 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         try {
-            Session session = Util.getSessionFactory().openSession();
             Transaction transaction = session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS USERS").executeUpdate();
             transaction.commit();
-            session.close();
             System.out.println("Таблица удалена.");
         } catch (HibernateException e) {
             e.printStackTrace();
@@ -64,13 +61,11 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
         try {
-            Session session = Util.getSessionFactory().openSession();
-            Transaction transaction = session.beginTransaction();
+            Transaction transaction = this.session.beginTransaction();
             User user = new User(name, lastName, age);
             if (user != null)
                 session.save(user);
             transaction.commit();
-            session.close();
             System.out.println("Пользователь: " + name + " " + lastName + " " + age + " лет добавлен в таблицу.");
         } catch (HibernateException e) {
             System.out.println("Ошибка добавления пользователя: " + name + " " + lastName + " " + age);
@@ -81,15 +76,14 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
+
         try {
-            Session session = Util.getSessionFactory().openSession();
-            Transaction transaction = session.beginTransaction();
+            Transaction transaction = this.session.beginTransaction();
             Object user = session.get(User.class, id);
             if (user != null) {
                 session.delete(user);
             }
             transaction.commit();
-            session.close();
             System.out.println("Удаление пользователя по id = " + id);
         } catch (HibernateException e) {
             System.out.println("Ошибка удаления пользователя.");
@@ -103,13 +97,12 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
 
-        Session session = Util.getSessionFactory().openSession();
-        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaBuilder builder = this.session.getCriteriaBuilder();
         Class<User> user = User.class;
         CriteriaQuery<User> query = builder.createQuery(user);
         Root<User> root = query.from(user);
         query.select(root);
-        Query<User> q = session.createQuery(query);
+        Query<User> q = this.session.createQuery(query);
         List<User> list = q.getResultList();
 
 
@@ -119,13 +112,11 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        try {
 
-            Session session = Util.getSessionFactory().openSession();
-            Transaction transaction = session.beginTransaction();
+        try {
+            Transaction transaction = this.session.beginTransaction();
             session.createSQLQuery("TRUNCATE TABLE USERS").executeUpdate();
             transaction.commit();
-            session.close();
             System.out.println("Таблица очищена.");
         } catch (HibernateException e) {
             e.printStackTrace();
